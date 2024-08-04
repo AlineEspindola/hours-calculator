d0fe5374e1bed9fe8e91267d58f3063052559e6b
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2024-08-04 13:10:43 -0300

    feat: Adiciona componente de operação

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.scss';
+import DropdownOperation from './components/DropdownOperation';
+import { Col, Flex, Row } from 'antd';
 
 function App() {
   return (
-    <>Calculadora de Horasa</>
+    <Row
+      justify="center"
+    >
+      <Col>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '35px' }}>
+          <DropdownOperation />
+          <DropdownOperation />
+        </div>
+      </Col>
+    </Row>
   );
 }
 
---
+++ src/components/DropdownOperation/index.tsx
@@ -0,0 +1,31 @@
+import { DownOutlined, MinusOutlined, PlusOutlined } from '@ant-design/icons';
+import { Dropdown, Button, Space, Menu } from 'antd';
+import { useState } from 'react';
+
+const DropdownOperation: React.FC = () => {
+  const [selectedOption, setSelectedOption] = useState<string>('sum');
+
+  const handleOptionClick = (e: { key: string }) => {
+    setSelectedOption(e.key);
+  };
+
+  const options = (
+    <Menu onClick={handleOptionClick}>
+      <Menu.Item key="sum" icon={<PlusOutlined />} />
+      <Menu.Item key="subtract" icon={<MinusOutlined />} />
+    </Menu>
+  );
+
+  return (
+    <Dropdown overlay={options}>
+      <Button>
+        <Space>
+          {selectedOption === 'sum' ? <PlusOutlined /> : <MinusOutlined />}
+          <DownOutlined />
+        </Space>
+      </Button>
+    </Dropdown>
+  );
+};
+
+export default DropdownOperation;
