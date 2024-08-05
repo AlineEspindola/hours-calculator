ec9f0b12d32dad5bc0bc839e6cab65a03b49f23d
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2024-08-04 23:14:59 -0300

    feat: Adiciona base da lógica dos inputs

--- src/components/InputTime/index.tsx
+++ src/components/InputTime/index.tsx
@@ -1,13 +1,47 @@
 import { InputNumber, Space } from "antd";
+import { useEffect, useState } from "react";
 
 const InputTime: React.FC = () => {
+  const [totalTime, setTotalTime] = useState<number>(0);
+  const [hour, setHour] =  useState<number>(0);
+  const [minute, setMinute] =  useState<number>(0);
+  const [second, setSecond] =  useState<number>(0);
+
+  const changeHour = (hour: number | null) => {
+    if (hour !== null) {
+      setHour(hour);
+    } else {
+      setHour(0);
+    }
+  }
+
+  const changeMinute = (minute: number | null) => {
+    if (minute !== null) {
+      setMinute(minute);
+    } else {
+      setMinute(0);
+    }
+  }
+
+  const changeSecond = (second: number | null) => {
+    if (second !== null) {
+      setSecond(second);
+    } else{
+      setSecond(0);
+    }
+  }
+
+  useEffect(() => {
+    setTotalTime((hour*3600)+(minute*60)+second);
+  }, [hour, minute, second, totalTime])
+
   return (
     <Space wrap>
-      <InputNumber size="large"  max={9999999} defaultValue={17} />
+      <InputNumber size="large" max={9999999} min={0} defaultValue={0} onChange={changeHour} />
       <>:</>
-      <InputNumber size="large"  max={59} defaultValue={34} />
+      <InputNumber size="large" max={59} min={0} defaultValue={0} onChange={changeMinute } />
       <>:</>
-      <InputNumber size="large"  max={59} defaultValue={12} />
+      <InputNumber size="large" max={59} min={0} defaultValue={0} onChange={changeSecond} />
     </Space>
   );
 }
