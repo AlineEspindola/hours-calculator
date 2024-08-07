0aea8bc142e5bb11867dd051ae4540f631afe822
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2024-08-06 22:08:11 -0300

    feat: Soma todos e converte

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 import DropdownOperation from './components/DropdownOperation';
 import InputTime from './components/InputTime';
@@ -9,10 +9,31 @@ function App() {
   const [totalSeconds, setTotalSeconds] = useState<number>(0);
   const [amountInputTime, setAmountInputTime] = useState<number>(2);
   const [amoutDropdownOperation, setAmoutDropdownOperation] = useState<number>(1);
+  const [inputValues, setInputValues] = useState<number[]>(Array(amountInputTime).fill(0));
 
   const addInputTime = () => {
     setAmountInputTime(amountInputTime + 1);
     setAmoutDropdownOperation(amoutDropdownOperation + 1);
+    setInputValues([...inputValues, 0]);
+  }
+
+  const handleInputChange = useCallback((index: number, value: number) => {
+    const newInputValues = [...inputValues];
+    newInputValues[index] = value;
+    setInputValues(newInputValues);
+    setTotalSeconds(newInputValues.reduce((acc, curr) => acc + curr, 0));
+
+  }, [inputValues]);
+
+  const converterSecondsToHours = (totalSeconds: number) => {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    if (hours > 9) {
+      return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
+    return `0${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   }
 
   return (
@@ -26,7 +47,7 @@ function App() {
           </div>
           <div className='calculator__inputs'>
             {Array.from({ length: amountInputTime }).map((_, index) => (
-              <InputTime key={index} onChange={setTotalSeconds} />
+              <InputTime key={index} onChange={(value) => handleInputChange(index, value)} />
             ))}
             <Button type="dashed" onClick={addInputTime}>
               Adicionar <PlusOutlined />
@@ -35,7 +56,7 @@ function App() {
         </div>
         <div className='result'>
           <Divider>Resultado</Divider>
-          <InputNumber size="large" readOnly value={totalSeconds} />
+          <InputNumber size="large" readOnly value={converterSecondsToHours(totalSeconds)} />
         </div>
       </div>
     </Flex>
